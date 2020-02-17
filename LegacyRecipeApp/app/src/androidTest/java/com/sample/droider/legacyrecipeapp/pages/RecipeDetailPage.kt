ac60595c6e67f99e00844c172f3b39c8a55b63e7
山田 遼 <ryo_yamada@waku-2.com>
2020-02-17 14:14:15 +0900

    画面ごとの処理を追加

---
+++ LegacyRecipeApp/app/src/androidTest/java/com/sample/droider/legacyrecipeapp/pages/RecipeDetailPage.kt
@@ -0,0 +1,7 @@
+package com.sample.droider.legacyrecipeapp.pages
+
+class RecipeDetailPage {
+
+    fun RecipeDetailPage(){
+    }
+}
---
+++ LegacyRecipeApp/app/src/androidTest/java/com/sample/droider/legacyrecipeapp/pages/RecipeListPage.kt
@@ -0,0 +1,20 @@
+package com.sample.droider.legacyrecipeapp.pages
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.sample.droider.legacyrecipeapp.R
+
+class RecipeListPage {
+
+    /**
+     * 指定した位置をタップする
+     * @param position position
+     */
+    fun clickToList(position: Int): RecipeDetailPage {
+        onView(withId(R.id.recycler)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(position, click()))
+        return RecipeDetailPage()
+    }
+}
